e5a6014e3c7b249bd5e31e56f35b72c7d927251b
kasiabasisty <61195876+kasiabasisty@users.noreply.github.com>
2020-02-26 19:56:08 +0100

    Update MultiplicationTable.java

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -3,41 +3,22 @@ package pl.coderstrust.multiplication;
 public class MultiplicationTable {
 
     public static void main(String[] args) {
-        printMultiplicationTable(8);
+        printMultiplicationTable(12);
     }
 
     public static void printMultiplicationTable(int size) {
-
-        System.out.print("      ");
+        System.out.printf("%4s", "");
 
         for (int i = 1; i <= size; i++) {
-            if (i >= 10) {
-                System.out.print(i);
-                System.out.print("     ");
-            } else {
-                System.out.print(i);
-                System.out.print("      ");
-            }
+            System.out.printf("%4s", i);
         }
 
         System.out.println();
 
         for (int i = 1; i <= size; i++) {
-
-            if (i < 10) {
-                System.out.print(i + "     ");
-            } else {
-                System.out.print(i + "    ");
-            }
-
+            System.out.printf("%4s", i);
             for (int j = 1; j <= size; j++) {
-                if (i * j >= 100) {
-                    System.out.print(j * i + "    ");
-                } else if (i * j < 100 && i * j >= 10) {
-                    System.out.print(j * i + "     ");
-                } else {
-                    System.out.print(j * i + "      ");
-                }
+                System.out.printf("%4s", i * j);
             }
             System.out.println();
         }
