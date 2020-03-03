827b0f562835d13c0f6d26ccdce0b844fb900370
Kasia Basisty <kbasisty@op.pl>
2020-03-03 16:22:20 +0100

    brackets added, parentheses removed

--- src/main/java/pl/coderstrust/pascaltriangle/PascalTriangle.java
+++ src/main/java/pl/coderstrust/pascaltriangle/PascalTriangle.java
@@ -7,9 +7,11 @@ public class PascalTriangle {
 
     public static void printPascalTriangle(int size) {
         for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size - i - 1; j++) System.out.printf("%3s", "");
+            for (int j = 0; j < size - i - 1; j++) {
+                System.out.printf("%3s", "");
+            }
             for (int j = 0; j <= i; j++) {
-                long term = (factorial(i) / (factorial(j) * factorial(i - j)));
+                long term = factorial(i) / (factorial(j) * factorial(i - j));
                 System.out.printf("%6d", term);
             }
             System.out.println();
