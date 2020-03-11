01c97f620020032722ae0876b8b7689247c14f2e
Kasia Basisty <kbasisty@op.pl>
2020-03-11 18:48:19 +0100

    particular methods created

--- src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
+++ src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
@@ -5,32 +5,50 @@ import java.util.Arrays;
 public class SieveOfEratosthenes {
 
     public static void main(String[] args) {
-        int[] finalArray = sieve(14);
+        int[] finalArray = sieve(13);
         System.out.println(Arrays.toString(finalArray));
     }
 
     public static int[] sieve(int maximumNumber) {
+        if (maximumNumber < 2) {
+            return new int[0];
+        }
+        int[] array = createInitialArray(maximumNumber);
+        int countOfNonPrimeNumbers = markNonePrimeNumbers(array);
+        int countOfPrimeNumbers = array.length - countOfNonPrimeNumbers;
+        return extractPrimesNumbers(array, countOfPrimeNumbers);
+    }
+
+    private static int[] createInitialArray(int maximumNumber) {
         int[] array = new int[maximumNumber + 1];
         for (int i = 0; i < maximumNumber + 1; i++) {
             array[i] = i;
         }
-        for (int i = 4; i <= maximumNumber; i++) {
-            for (int j = 2; j <= maximumNumber + 1; j++) {
+        return array;
+    }
+
+    private static int markNonePrimeNumbers(int[] array) {
+        for (int i = 4; i <= array.length - 1; i++) {
+            for (int j = 2; j <= array.length; j++) {
                 if (array[i] % j == 0 && array[i] != j) {
                     array[i] = 0;
                 }
             }
         }
-        int numberOfPrimaryNumbers = 0;
-        for (int i = 0; i < maximumNumber + 1; i++) {
-            if (array[i] != 0) {
-                numberOfPrimaryNumbers++;
+        int countOfNonPrimeNumbers = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                countOfNonPrimeNumbers++;
             }
         }
-        int[] primeNumbersArray = new int[numberOfPrimaryNumbers];
-        for (int i = 0; i < numberOfPrimaryNumbers; i++) {
-            primeNumbersArray[i] = i;
-        }
+        return countOfNonPrimeNumbers;
+    }
+
+    private static int[] extractPrimesNumbers(int[] array, int countOfPrimeNumbers) {
+        int[] primeNumbersArray = new int[countOfPrimeNumbers];
+//        for (int i = 0; i < countOfPrimeNumbers; i++) {
+//        primeNumbersArray[i] = i;
+//    }
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < array.length - 1; j++) {
                 if (array[j] == 0) {
