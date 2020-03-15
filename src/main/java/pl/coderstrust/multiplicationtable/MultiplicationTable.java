8f2235da10cd08ef487a7fb90385e271ee3373ff
Kasia Basisty <kbasisty@op.pl>
2020-03-15 19:27:23 +0100

    multiplication_test initial commit

--- src/main/java/pl/coderstrust/multiplicationtable/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplicationtable/MultiplicationTable.java
@@ -2,21 +2,35 @@ package pl.coderstrust.multiplicationtable;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        printMultiplicationTable(12);
+        int[][] multiplicationTable = getMultiplicationTable(5);
+        printMultiplicationTable(multiplicationTable);
     }
 
-    public static void printMultiplicationTable(int size) {
-        System.out.printf("%4s", "");
-        for (int i = 1; i <= size; i++) {
-            System.out.printf("%4d", i);
+    public static int[][] getMultiplicationTable(int size) {
+        if (size < 0) {
+            throw new IllegalArgumentException("Size can't be lower than zero!");
         }
-        System.out.println();
+        int[][] multiplicationTable = new int[size + 1][size + 1];
+
+        for (int i = 0; i <= size; i++) {
+            multiplicationTable[i][0] = i;
+            multiplicationTable[0][i] = i;
+        }
         for (int i = 1; i <= size; i++) {
-            System.out.printf("%4d", i);
             for (int j = 1; j <= size; j++) {
-                System.out.printf("%4d", i * j);
+                multiplicationTable[i][j] = i * j;
+            }
+        }
+        return multiplicationTable;
+    }
+
+    public static void printMultiplicationTable(int[][] multiplicationTable) {
+        for (int i = 0; i < multiplicationTable.length; i++) {
+            for (int j = 0; j < multiplicationTable.length; j++) {
+                System.out.printf("%4d", multiplicationTable[i][j]);
             }
             System.out.println();
         }
     }
 }
+
---
+++ src/test/java/pl/coderstrust/multiplicationtable/MultiplicationTableTest.java
@@ -0,0 +1,30 @@
+package pl.coderstrust.multiplicationtable;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class MultiplicationTableTest {
+
+    @Test
+    public void shouldReturnCorrectResult() {
+        //Given
+        int input = 5;
+        int[][] expected = {{0, 1, 2, 3, 4, 5}, {1, 1, 2, 3, 4, 5}, {2, 2, 4, 6, 8, 10}, {3, 3, 6, 9, 12, 15},
+                {4, 4, 8, 12, 16, 20}, {5, 5, 10, 15, 20, 25}};
+
+        //When
+        int[][] actual = MultiplicationTable.getMultiplicationTable(input);
+
+        //Then
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void shouldTrowExceptionForInvalidSize() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> MultiplicationTable.getMultiplicationTable(-1));
+        assertThat(exception.getMessage(), is("Size can't be lower than zero!"));
+    }
+}
