8b6015716192d3af796824a1f250d0c040bc0e15
Kasia <kbasisty@op.pl>
2020-06-02 19:19:53 +0200

    work in progress

--- src/test/java/pl/coderstrust/numbersfromfile/IntegrationTest.java
+++ src/test/java/pl/coderstrust/numbersfromfile/IntegrationTest.java
@@ -3,34 +3,20 @@ package pl.coderstrust.numbersfromfile;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 class IntegrationTest {
 
     @Test
-    void processLine() {
-
+    void processLine() throws IOException {
+        // given
         NumbersProcessor numbersProcessor = new NumbersProcessor();
         FileProcessor fileProcessor = new FileProcessor();
+        Processor processor = new Processor(numbersProcessor, fileProcessor);
 
-         public Processor(numbersProcessor, fileProcessor) {
-            this.numbersProcessor = numbersProcessor;
-            this.fileProcessor = fileProcessor;
-         }
+        // when
+        processor.process("src/main/resources/1000.txt", "src/main/resources/1000_result.txt");
 
-        public void process(String filePath, String resultFilePath) throws IOException {
-            List<String> linesFromFile = fileProcessor.readLinesFromFile(filePath);
-            List<String> resultLines = new ArrayList<>();
-            for (String line : linesFromFile) {
-                try (Scanner scanner = new Scanner(line)) {
-                    if (line.matches("(\\s*\\d+\\s*)+")) {
-                        resultLines.add(numbersProcessor.processLine(line));
-                    }
-                }
-            }
-            fileProcessor.writeLinesToFile(resultLines, resultFilePath);
-        }
+        // then
+
     }
 }
